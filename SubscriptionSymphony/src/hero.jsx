85bacadca77885c1b03e7b5b40e25c99f8e4f7a2
Sekiryuutei-101 <jitsaha567@gmail.com>
2025-04-06 09:44:09 +0530

    added spline

--- SubscriptionSymphony/src/hero.jsx
+++ SubscriptionSymphony/src/hero.jsx
@@ -1,10 +1,13 @@
-import Spline from '@splinetool/react-spline';
+import Design from './spline';
 import "./css/hero.css"
 function Hero() {
     return (
-        <div className="flex justify-center items-center h-screen w-full bg-[D9D9D9]">
-            <div className="max-w-4xl text-center">
-                <h1 className="font-bold text-5xl text-black">Subscription <br /> Symphony</h1>
+        <div className="relative flex justify-center items-center h-screen w-full bg-[D9D9D9]">
+            <div className="absolute inset-0 z-0">
+                <Design/>
+            </div>
+            <div className="z-10 max-w-4xl text-center relative">
+                <h1 className="font-bold text-5xl text-white">Subscription <br /> Symphony</h1>
                 <button className="mt-8 bg-black text-white py-3 px-6 rounded-full hover:bg-gray-800 transition duration-300">Sign Up</button>
             </div>
         </div>
---
+++ SubscriptionSymphony/src/spline.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Design() {
+  return (
+    <Spline scene="https://prod.spline.design/QBVexwFsiIUE50ka/scene.splinecode" />
+  );
+}
